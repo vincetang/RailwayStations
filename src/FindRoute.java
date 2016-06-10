47a3b058139025b6658e3f2bdec9cfbb3ff0d15f
Vincent Tang <Vince@MacBook-Pro.local>
2016-06-10 17:00:54 -0400

    initial commit - stations class made

---
+++ src/FindRoute.java
@@ -0,0 +1,19 @@
+
+public class FindRoute {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// read in file line by ilne
+		// if stations don't exist
+		// 	create stations
+		// Add stations to each other's routes
+		// 
+		// prompt user input for two stations (start, end)
+		// run djikstra's algorithm to find shortest path
+		
+	}
+
+}
---
+++ src/Station.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Station {
+	private String stationName;
+	private HashMap<String, Integer> routes;
+	
+	
+	public Station(String name, HashMap<String, Integer> routes) {
+		this.stationName = name;
+		this.routes = routes;
+	}
+	
+	public String getStationName() {
+		return stationName;
+	}
+
+	public Integer getDistanceTo(String station) {
+		if (this.routes.containsKey(station)) {
+			return this.routes.get(station);
+		}
+		return -1;
+	}
+	
+	public boolean hasRoutesToOtherStations() {
+		return (this.routes.size() != 0);
+	}
+	
+	public ArrayList<String> getConnectedStations() {
+		ArrayList<String> stationNames = new ArrayList<String>();
+		stationNames.addAll(this.routes.keySet());
+		return stationNames;
+	}
+	
+	public Map.Entry<String, Integer> getClosestStation() {
+		Iterator iter = this.routes.entrySet().iterator();
+		Map.Entry<String, Integer> closestStation = null;
+		Integer shortestDist = -1;
+		while (iter.hasNext()) {
+			Map.Entry<String, Integer> route = (Map.Entry<String, Integer>) iter.next();
+			if (shortestDist == -1 || route.getValue() < shortestDist) {
+				shortestDist = route.getValue();
+				closestStation = route;
+			}
+		}
+		return closestStation;
+	}
+	
+
+}
